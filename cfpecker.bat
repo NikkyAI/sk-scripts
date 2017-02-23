71c68051547afa7a568dac99c6a79601ee392b75
NikkyAi <root@nikky.moe>
2017-02-23 18:16:00 +0100

    remove copying local files from scripts

--- cfpecker.bat
+++ cfpecker.bat
@@ -4,7 +4,3 @@ git -C cfpecker pull || git clone https://github.com/NikkyAI/cfpecker.git cfpeck
 
 py -3 cfpecker/bin/cfpecker.py
 
-:: TODO make local files work and avoid this copying
-
-cd modpacks\test_pack
-robocopy local src\mods /s /e
